0eef362d81dd518ea8c34d75be830a5544f092e4
ACosmicCake <acosmiccake@gmail.com>
2025-06-03 14:03:40 -0400

    commit

--- computer_use_demo/loop.py
+++ computer_use_demo/loop.py
@@ -5,7 +5,7 @@ Agentic sampling loop that calls the Anthropic API and local implementation of a
 import platform
 from collections.abc import Callable
 from datetime import datetime
-from enum import StrEnum
+from strenum import StrEnum # Corrected import for Py<3.11
 from typing import Any, cast
 
 import httpx
@@ -51,14 +51,14 @@ class APIProvider(StrEnum):
 # environment it is running in, and to provide any additional information that may be
 # helpful for the task at hand.
 SYSTEM_PROMPT = f"""<SYSTEM_CAPABILITY>
-* You are utilising an Ubuntu virtual machine using {platform.machine()} architecture with internet access.
+* You are utilising an Windows virtual machine using {platform.machine()} architecture with internet access.
 * You can feel free to install Ubuntu applications with your bash tool. Use curl instead of wget.
 * To open firefox, please just click on the firefox icon.  Note, firefox-esr is what is installed on your system.
 * Using bash tool you can start GUI applications, but you need to set export DISPLAY=:1 and use a subshell. For example "(DISPLAY=:1 xterm &)". GUI apps run with bash tool will appear within your desktop environment, but they may take some time to appear. Take a screenshot to confirm it did.
 * When using your bash tool with commands that are expected to output very large quantities of text, redirect into a tmp file and use str_replace_based_edit_tool or `grep -n -B <lines before> -A <lines after> <query> <filename>` to confirm output.
 * When viewing a page it can be helpful to zoom out so that you can see everything on the page.  Either that, or make sure you scroll down to see everything before deciding something isn't available.
 * When using your computer function calls, they take a while to run and send back to you.  Where possible/feasible, try to chain multiple of these calls all into one function calls request.
-* The current date is {datetime.now().strftime('%Y-%m-%d')}.
+* The current date is {datetime.today().strftime(f"%A, %B {datetime.today().day}, %Y")}.
 </SYSTEM_CAPABILITY>
 
 <IMPORTANT>
